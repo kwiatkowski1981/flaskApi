9d7eff63149481843fd6280b9fd0d4c4eb84ef41
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-06-26 22:53:41 +0200

    added api and tests dir

---
+++ api/authors.py
@@ -0,0 +1,11 @@
+def index():
+    return 'index'
+
+
+def add():
+    return 'add'
+
+
+def delete():
+    return 'delete'
+
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from api import authors
 
 
 app = Flask(__name__)
+app.add_url_rule('/authors', view_func=authors.index, methods=['GET'])
+app.add_url_rule('/authors', view_func=authors.add, methods=['POST'])
+app.add_url_rule('/authors/<id>', view_func=authors.delete, methods=['DELETE'])
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!!!'
+# @app.route('/')
+# def hello():
+#     return 'Hello World!!!'
+
+
 
