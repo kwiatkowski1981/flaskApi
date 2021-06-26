8864e289a9a321af49bd43416b60b12bd7612c4d
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-06-27 01:01:53 +0200

    updated links in my tests which wasn't good

--- tests/test_authors.py
+++ tests/test_authors.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_get_authors():
-    response = requests.get('http://http://localhost:5000/authors')
+    response = requests.get('http://localhost:5000/authors')
     assert response.status_code == 200
 
 
@@ -14,19 +14,19 @@ def test_add_author():
     }
 
     # when
-    response = requests.post('http://http://localhost:5000/authors', payload)
+    response = requests.post('http://localhost:5000/authors', payload)
 
     # then
     assert response.status_code == 201
 
-    response = requests.get('http://http://localhost:5000/authors')
+    response = requests.get('http://localhost:5000/authors')
     data = response.json()
     assert data[-1].first_name == 'Grzegorz'
     assert data[-1].last_name == 'Brzęczyszczykiewicz'
 
 
 def test_delete_author():
-    response = requests.get('http://http://localhost:5000/authors')
+    response = requests.get('http://localhost:5000/authors')
     data = response.json()
     quantity = len(data)
     last_id = data[-1].id
