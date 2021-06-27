bf3905a0e32ea0f1c731172e8ab9a39af261dd5d
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-06-28 00:02:04 +0200

    added, added to many things to add and list in one commit, TODO: learn how to use git properly :P

--- api/authors.py
+++ api/authors.py
@@ -1,9 +1,32 @@
+from flask import Response, request
+from db import get_connection
+from json import dumps, loads
+
+
 def index():
-    return 'index'
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT id, first_name, last_name FROM authors')
+
+    return Response(dumps(cursor.fetchall()), mimetype='application/json')
 
 
 def add():
-    return 'add'
+    data = loads(request.data.decode('utf-8'))
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        'INSERT INTO authors(first_name, last_name) VALUES(%s, %s) RETURNING id',
+        (data['first_name'], data['last_name'])
+    )
+    author_id = cursor.fetchone()[0]
+    connection.commit()
+
+    return Response(dumps({
+        'id': author_id
+    }), mimetype='application/json')
 
 
 def delete():
---
+++ db.py
@@ -0,0 +1,20 @@
+import psycopg2
+from flask import g
+
+
+def init_app(app):
+    app.teardown_appcontext(close_connection)
+
+
+def get_connection():
+    if 'connection' not in g:
+        g.connection = psycopg2.connect(dbname="app", user="app", password="admin123", host="db")
+
+    return g.connection
+
+
+def close_connection():
+    connection = g.pop('connection', None)
+    if connection is not None:
+        connection.close()
+
